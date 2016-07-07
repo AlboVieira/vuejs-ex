d48149102f706178e7e2a6e9b0fbba8c6a733be9
Albo Vieira <albovieira@gmail.com>
2016-07-06 21:58:29 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//starts a new app with vue
+var hello = new Vue({
+
+    //the element that I will use
+    el:'#hello',
+    data: { // all data that I can use in vue app
+        msg: 'Hello World'
+    }
+
+
+});
+
