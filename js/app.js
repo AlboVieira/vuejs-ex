e0db6fe4a160db9f17bf3f4df43cf91a2abaec1a
Albo Vieira <albovieira@gmail.com>
2016-07-06 22:58:48 -0300

    vue testing

--- js/app.js
+++ js/app.js
@@ -4,7 +4,28 @@ var hello = new Vue({
     //the element that I will use
     el:'#hello',
     data: { // all data that I can use in vue app
-        msg: 'Hello World'
+        msg: 'Hello World',
+        list: [
+            {name: "Maria"},
+            {name: "Joao"},
+            {name: "Pedro"}
+        ],
+        newElement:'',
+        elements:[]
+    },
+    methods:{
+        addElement: function () {
+            var title = this.newElement.trim();
+            if(title ) {
+                this.elements.push({title:title});
+                this.newElement = '';
+            }
+
+        },
+        removeElement: function(e,index){
+            e.preventDefault();
+            this.elements.splice(index,1);
+        }
     }
 
 
